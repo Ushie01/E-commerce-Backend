ac560740a5ab228905f9d2fd821de8f56d9f0b56
Ushie01 <abugodwinaj@gmail.com>
2023-01-16 15:08:36 +0100

    Updating package.json

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const app = require('./index')
 const dotenv = require('dotenv');
 
-
 // CONNECTING TO MONGODB SERVER
 dotenv.config({ path: './config.env' })
-const DB = process.env.DATABASE_PASSWORD;
-
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
@@ -14,7 +12,6 @@ mongoose.connect(DB, {
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(con => console.log('DB connection successful'))
-
 
 const port = process.env.PORT || 3000;
 
